3114a77470bf9c19e6fe29ee3f727b4e31d0418b
hsusteph5 <hsusteph5@gmail.com>
2018-09-19 17:32:41 -0700

    Added session

--- railslite/lib/session.rb
+++ railslite/lib/session.rb
@@ -4,16 +4,27 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    #req = Rack::Request 
+    all_cookies = req.cookies
+    @cookie = JSON.parse(all_cookies["_rails_lite_app"]) if all_cookies["_rails_lite_app"]
+    p @cookie
+    @cookie ||= {}
   end
 
   def [](key)
+    @cookie[key]
+    
   end
 
   def []=(key, val)
+    @cookie[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    path = "\\"
+    value = 
+    res.set_cookie("_rails_lite_app", "") 
   end
 end
